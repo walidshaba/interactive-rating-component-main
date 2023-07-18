1d310833afcd3b7460ce866a170da9bff8621b51
Musa Shaba <musamohshaba@gmail.com>
2023-07-18 10:54:54 +0100

    saving, till we meet again on thursday

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,14 @@
+let a = 0;
 const submit = document.querySelector('button');
+const form = document.getElementById('form');
+const thanks = document.getElementById('thanks');
+const mainPage = document.getElementById('main');
+const radio = document.querySelector('input[ name="ratevalue"]:checked');
 
-submit.addEventListener('form', (e) => {});
+form.addEventListener('submit', (e) => {
+  if (radio.checked) {
+    thanks.classList.toggle('active');
+  } else {
+    e.preventDefault();
+  }
+});
